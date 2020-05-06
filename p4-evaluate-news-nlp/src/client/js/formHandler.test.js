f102b9e63389f6cae7cb70bece753685c69de5f3
Yousof <yousofebneddin@yahoo.com>
2020-05-05 23:46:12 -0700

    I cannot jest a function that has fetch

--- p4-evaluate-news-nlp/src/client/js/formHandler.test.js
+++ p4-evaluate-news-nlp/src/client/js/formHandler.test.js
@@ -1,6 +1,5 @@
 // for async/await tests
 import 'regenerator-runtime/runtime'
-import "isomorphic-fetch" // to test fetch calls??
 
 test('sentiment_analysis_get', async function(){
   var formHandler = require('./formHandler');
